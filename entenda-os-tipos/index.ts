182a418dfe80418cdef6759f1ab703b28ccf2639
WRyan <wesley.ryan03@gmail.com>
2024-11-21 20:33:51 -0300

    Add files via upload

---
+++ entenda-os-tipos/index.ts
@@ -0,0 +1,8 @@
+function sendMessage(userName: string, isLoggged: boolean) {
+    console.log(`Olá ${userName}. Você ${isLoggged ? "não" : ""} pode logar.`);
+  }
+  
+  const userName = "123"
+  const isLoggged = true
+  
+  sendMessage(userName, isLoggged)
