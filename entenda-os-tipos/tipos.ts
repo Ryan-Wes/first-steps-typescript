08eb730cf18154ac5751603b8e724621f33f2b73
WRyan <wesley.ryan03@gmail.com>
2024-11-21 20:50:21 -0300

    Add files via upload

---
+++ entenda-os-tipos/tipos.ts
@@ -0,0 +1,20 @@
+// TIPOS DE PRIMITIVOS
+
+let idades: number[] = [12, 22, 39];
+let nomes: string[] = ['Roberto', 'Ricardo', 'Raphael'];
+
+let idade: Array<number> = [12, 22, 39]
+
+let obj: any = {x:0};
+
+
+// TIPOS DE OBJETOS
+
+function imprimirCoordenadas(pt: {x: number; y: number}) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);    
+}
+
+imprimirCoordenadas({ x: 3, y: 7 });
+
+let obj: {x: number; y: number} = { x: 3, y: 7};
