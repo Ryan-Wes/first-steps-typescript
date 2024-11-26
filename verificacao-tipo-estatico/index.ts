3bad00cc9c965202e25682aa64669ac875c08600
WRyan <wesley.ryan03@gmail.com>
2024-11-26 08:45:52 -0300

    Add files via upload

---
+++ verificacao-tipo-estatico/index.ts
@@ -0,0 +1,13 @@
+function sum(number1: number, number2: number){
+    return number1 + number2;
+}
+
+sum(1, 2)
+
+// _________________________________________________________________________________________________
+
+const movies = ['Batman VS Superman', 'Senhor dos Anéis'];
+
+movies.forEach(item => {
+
+});
