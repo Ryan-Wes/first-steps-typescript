c2237ab150bb22d040b9a571d0a085fd267f013f
WRyan <wesley.ryan03@gmail.com>
2024-11-26 08:59:17 -0300

    Add files via upload

---
+++ tuples/index.ts
@@ -0,0 +1,7 @@
+const stringNumber: [string, number] = ["Roberto", 1];
+
+// _____________________________________________________________________________________
+
+let color: [number, number, number] = [255, 0, 0];
+
+let bgColor: [number, number, number, number?] = [255, 0, 0];
