70ec3c18e2af7d5776b1435fff608a67029f0b90
WRyan <wesley.ryan03@gmail.com>
2024-11-26 08:34:24 -0300

    Add files via upload

--- exemplo-compilacao/index.ts
+++ exemplo-compilacao/index.ts
@@ -2,7 +2,7 @@ function sendMessage(userName: string, isLoggged: boolean) {
   console.log(`Olá ${userName}. Você ${isLoggged ? "não" : ""} pode logar.`);
 }
 
-const userName = "Beto"
-const isLoggged = true
+const userName = "Beto";
+const isLoggged = true;
 
-sendMessage(userName, isLoggged)
+sendMessage(userName, isLoggged);
---
+++ tipagem-explicita-e-inferencia/index.ts
@@ -0,0 +1,2 @@
+let nome = 123;
+nome = 123;
