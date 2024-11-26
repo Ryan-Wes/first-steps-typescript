af22156cec26dd847fd1b1339ce401f7884ba814
WRyan <wesley.ryan03@gmail.com>
2024-11-26 09:40:23 -0300

    Add files via upload

---
+++ any/index.ts
@@ -0,0 +1,9 @@
+let moviesCount:any = 10;
+
+moviesCount = '10';
+
+function sum(number1: any, number2: any) {
+    return number1 + number2;
+}
+
+console.log(sum(10, 20))
--- verificacao-tipo-estatico/index.ts
+++ verificacao-tipo-estatico/index.ts
@@ -1,8 +1,8 @@
-function sum(number1: number, number2: number){
-    return number1 + number2;
-}
+// function sum(number1: number, number2: number){
+//     return number1 + number2;
+// }
 
-sum(1, 2)
+// sum(1, 2)
 
 // _________________________________________________________________________________________________
 
