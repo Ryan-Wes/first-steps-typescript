25af5eb126406f2e4797d06dfbaf43a0024e9cff
WRyan <wesley.ryan03@gmail.com>
2024-11-28 10:30:09 -0300

    Add files via upload

---
+++ tipagem-em-objetos/index.ts
@@ -0,0 +1,9 @@
+type Custumer =  {
+    name: string;
+    age: number;
+}
+
+function showCostumerInfo(custumer: Custumer) {
+    console.log(`O nome do cliente é: ${custumer.name}`);
+    console.log(`A idade do cliente é: ${costumer.age}`);  
+}
